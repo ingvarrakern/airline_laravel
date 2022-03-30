597e4f4f8514a770c10f90c8460200ed8ccd705e
Elena Kirova <ingvarrakern@gmail.com>
2022-03-30 11:48:53 +0500

    table of users

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,36 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
+                    {{ __('You are logged in! '. Auth::user()->name) }}
+                </div>                
+            </div>
+            <div class="card">
+                <div class="card-header">{{ __('User info') }}</div>
 
-                    {{ __('You are logged in!') }}
-                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Date of creation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">{{ __(Auth::user()->id) }}</th>
+                                <td>{{ __(Auth::user()->name) }}</td>
+                                <td>{{ __(Auth::user()->email) }}</td>
+                                <td>{{ __(Carbon\Carbon::parse(Auth::user()->created_at)->format('d-m-Y h:m:s'))}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>                
             </div>
         </div>
     </div>
